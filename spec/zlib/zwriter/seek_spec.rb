35afa2b1a65274af776dc0691a9be16d4510a480
Jeremy Bopp <jeremy@bopp.net>
2009-04-25 15:10:05 -0500

    Added tests for Zlib::ZWriter#seek

---
+++ spec/zlib/zwriter/seek_spec.rb
@@ -0,0 +1,52 @@
+require File.dirname(__FILE__) + '/../../../spec_helper'
+require File.dirname(__FILE__) + '/../fixtures/classes'
+require 'archive/support/zlib'
+require 'stringio'
+
+describe "Zlib::ZWriter#seek" do
+  it "can seek to the beginning of the stream when the delegate responds to rewind" do
+    compressed_data = StringIO.new
+    Zlib::ZWriter.open(compressed_data) do |c|
+      c.write('test')
+      lambda { c.seek(0) }.should_not raise_error
+    end
+  end
+
+  it "raises Errno::EINVAL when attempting to seek to the beginning of the stream when the delegate does not respond to rewind" do
+    delegate = mock('delegate')
+    # RSpec's mocking facility supposedly supports this, but MSpec's does not as
+    # of version 1.5.10.
+    #delegate.should_receive(:write).with(an_instance_of(String)).at_least(:once).and_return { |s| s.length }
+    # Use the following instead for now.
+    delegate.should_receive(:write).at_least(:once).and_return(1)
+    Zlib::ZWriter.open(delegate) do |c|
+      lambda { c.seek(0) }.should raise_error(Errno::EINVAL)
+    end
+  end
+
+  it "raises Errno::EINVAL when seeking forward or backward from the current position of the stream" do
+    compressed_data = StringIO.new
+    Zlib::ZWriter.open(compressed_data) do |c|
+      c.write('test')
+      lambda { c.seek(1, IO::SEEK_CUR) }.should raise_error(Errno::EINVAL)
+      lambda { c.seek(-1, IO::SEEK_CUR) }.should raise_error(Errno::EINVAL)
+    end
+  end
+
+  it "raises Errno::EINVAL when seeking a non-zero offset relative to the beginning of the stream" do
+    compressed_data = StringIO.new
+    Zlib::ZWriter.open(compressed_data) do |c|
+      lambda { c.seek(-1, IO::SEEK_SET) }.should raise_error(Errno::EINVAL)
+      lambda { c.seek(1, IO::SEEK_SET) }.should raise_error(Errno::EINVAL)
+    end
+  end
+
+  it "raises Errno::EINVAL when seeking relative to the end of the stream" do
+    compressed_data = StringIO.new
+    Zlib::ZWriter.open(compressed_data) do |c|
+      lambda { c.seek(0, IO::SEEK_END) }.should raise_error(Errno::EINVAL)
+      lambda { c.seek(-1, IO::SEEK_END) }.should raise_error(Errno::EINVAL)
+      lambda { c.seek(1, IO::SEEK_END) }.should raise_error(Errno::EINVAL)
+    end
+  end
+end
